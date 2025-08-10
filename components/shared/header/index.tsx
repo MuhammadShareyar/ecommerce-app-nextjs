68e584a3236917a802106a635a2b8db1c489d882
Muhammad Shareyar <muhammad.shareyar@yassir.com>
2025-08-10 13:00:49 +0200

    header and footer component

---
+++ components/shared/header/index.tsx
@@ -0,0 +1,39 @@
+import { Button } from "@/components/ui/button";
+import { APP_NAME } from "@/lib/constants";
+import { ShoppingCartIcon, UserIcon } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="w-full border-b">
+      <div className="wrapper flex-between">
+        <div className="flex-start">
+          <Link href="/" className="flex-start">
+            <Image
+              src="/images/logo.svg"
+              priority={true}
+              alt={`${APP_NAME} logo`}
+              width={48}
+              height={48}
+            />
+
+            <span className="hidden lg:block font-bold text-2xl ml-3">
+              {APP_NAME}
+            </span>
+          </Link>
+        </div>
+        <div className="space-x-2">
+          <Button asChild variant="ghost">
+            <Link href="/cart"><ShoppingCartIcon/>Cart</Link>
+          </Button>
+          <Button asChild variant="ghost">
+            <Link href="/sign-in"><UserIcon/>Login</Link>
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
