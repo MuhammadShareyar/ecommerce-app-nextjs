945a7479508d6ed15db1e4f36c77e4f4e3e67179
Muhammad Shareyar <muhammad.shareyar@yassir.com>
2025-08-10 15:44:06 +0200

    product card and displaying dummy data

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,10 @@
+import ProductList from "@/components/shared/product/product-list";
+import sampleData from "@/sample-data";
+
 const HomePage = () => {
   return (
     <>
-      <h1>Home Page</h1>
+      <ProductList data={sampleData.products} title="new arrival" limit={6}/>
     </>
   );
 };
