d9303d5ecb00d4c02378c117e0d867abff9de73a
Muhammad Shareyar <shareyarz40@gmail.com>
2025-08-10 22:15:01 +0200

    product detail page

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -6,11 +6,16 @@ import { convertToPlainObject } from "../utils";
 
 // Get latest products
 export async function getLatestProducts() {
-
   const products = await prisma.product.findMany({
     take: LATEST_PRODUCT_LIMIT,
     orderBy: { createdAt: "desc" },
   });
 
   return convertToPlainObject(products);
+}
+
+// Get product by slug
+export async function getProductBySlug(slug: string) {
+  const product = await prisma.product.findFirst({ where: { slug: slug } });
+  return product;
 }
