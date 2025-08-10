7239e850a14fb2f24e01c7661458ec8e0a2b784a
Muhammad Shareyar <shareyarz40@gmail.com>
2025-08-10 21:56:49 +0200

    setup neon serverless setup for database

--- lib/actions/product.actions.ts
+++ lib/actions/product.actions.ts
@@ -1,12 +1,11 @@
 "use server";
 
+import { prisma } from "@/db/prisma";
 import { LATEST_PRODUCT_LIMIT } from "../constants";
-import { PrismaClient } from "../generated/prisma";
 import { convertToPlainObject } from "../utils";
 
 // Get latest products
 export async function getLatestProducts() {
-  const prisma = new PrismaClient();
 
   const products = await prisma.product.findMany({
     take: LATEST_PRODUCT_LIMIT,
