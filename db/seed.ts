618d604c39dbe53972b5153e932f06d80002f10b
Muhammad Shareyar <shareyarz40@gmail.com>
2025-08-10 22:43:42 +0200

    fix for prisma import

--- db/seed.ts
+++ db/seed.ts
@@ -1,9 +1,7 @@
-import { PrismaClient } from "@/lib/generated/prisma";
 import sampleData from "@/sample-data";
+import { prisma } from "./prisma";
 
 async function main() {
-  const prisma = new PrismaClient();
-
   await prisma.product.deleteMany();
 
   await prisma.product.createMany({ data: sampleData.products });
