a2447d527281cf23ef57e97085e824319050d470
Muhammad Shareyar <shareyarz40@gmail.com>
2025-08-12 14:37:08 +0200

    set middleware and set session cart id cookie

--- auth.ts
+++ auth.ts
@@ -4,6 +4,7 @@ import { prisma } from "./db/prisma";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { compareSync } from "bcrypt-ts-edge";
 import type { NextAuthConfig } from "next-auth";
+import { NextResponse } from "next/server";
 
 export const config = {
   pages: {
@@ -86,6 +87,28 @@ export const config = {
         }
       }
       return token;
+    },
+
+    authorized({ request, auth }) {
+      if (!request.cookies.get("sessionCartId")) {
+        // Generate cart cookie
+        const sessionCartId = crypto.randomUUID();
+
+        // Clone the request headers
+        const newRequestHeaders = new Headers(request.headers);
+
+        // Create a new response and add the new headers
+        const response = NextResponse.next({
+          request: {
+            headers: newRequestHeaders,
+          },
+        });
+
+        response.cookies.set("sessionCardId", sessionCartId);
+        return response;
+      } else {
+        return true;
+      }
     },
   },
 } satisfies NextAuthConfig;
