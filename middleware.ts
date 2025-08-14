4fa0d0ba9067e5c6cfb52bf61ac2adacfabd57b0
Muhammad Shareyar <shareyarz40@gmail.com>
2025-08-14 17:41:48 +0200

    update middleware for server config

--- middleware.ts
+++ middleware.ts
@@ -1,1 +1,34 @@
-export { auth as middleware } from "@/auth";
+import { auth } from "@/auth";
+import { NextResponse } from "next/server";
+
+export default auth((request) => {
+  const protectedPaths = [
+    /\/shipping-address/,
+    /\/payment-method/,
+    /\/place-order/,
+    /\/profile/,
+    /\/user\/(.*)/,
+    /\/order\/(.*)/,
+    /\/admin/,
+  ];
+
+  const isProtected = protectedPaths.some((p) =>
+    p.test(request.nextUrl.pathname)
+  );
+
+  if (!request.auth && isProtected) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+});
+
+export const config = {
+  matcher: [
+    "/shipping-address",
+    "/payment-method",
+    "/place-order",
+    "/profile",
+    "/user/:path*",
+    "/order/:path*",
+    "/admin/:path*",
+  ],
+};
